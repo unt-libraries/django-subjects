0da8ed760f40342c8d1d9eebed18fab720d23777
madhulika95b <madhulika.95b@gmail.com>
2020-10-30 15:25:07 -0500

    Modify some unit tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -9,35 +9,47 @@ import os
 class TestSubjectViews:
 
 	def test_viewall_subjects(self, client):
-		factories.SubjectFactory.create()
+		s1 = factories.SubjectFactory.create()
+		s2 = factories.SubjectFactory.create()
+		s3 = factories.SubjectFactory.create()
 		response = client.get(reverse('list'))
 		assert response.status_code == 200
 		assert response.templates[0].name == 'subjects/subject_list.html'
+		assert '<a style="display:block;" href="/">{s1} - {s2} - {s3}</a>'\
+			.format(s1=s1.name, s2=s2.name, s3=s3.name).encode() in response.content
 
 	def test_browse_subjects(self, client):
-		factories.SubjectFactory.create()
+		s1 = factories.SubjectFactory.create()
+		s2 = factories.SubjectFactory.create()
 		response = client.get(reverse('main_browse'))
 		assert response.status_code == 200
 		assert response.templates[0].name == 'subjects/subject_browse.html'
+		assert "<a href=\'/subjects/browse/{id}/\'>{name}</a>".format(id=s1.id, name=s1.name).encode() in response.content
+		assert "<a href=\'/subjects/browse/{id}/\'>{name}</a>".format(id=s2.id, name=s2.name).encode() in response.content
 
 	def test_browse_subjects_with_sub_id(self, client):
 		sub = factories.SubjectFactory.create()
 		response = client.get(reverse('browse', kwargs={'sub_id': sub.id}))
 		assert response.status_code == 200
 		assert response.templates[0].name == 'subjects/subject_browse.html'
+		assert "<input name=\'subject\' type=\'text\' size=\'57\' value=\'{name}\'>".format(
+			name=sub.name).encode() in response.content
 
 	def test_browse_subjects_with_sub_name(self, client):
 		sub = factories.SubjectFactory.create()
 		response = client.get(reverse('browse_sub_name', kwargs={'sub_name': sub.name}))
 		assert response.status_code == 200
 		assert response.templates[0].name == 'subjects/subject_browse.html'
+		assert "<input name=\'subject\' type=\'text\' size=\'57\' value=\'{name}\'>".format(
+			name=sub.name).encode() in response.content
 
 	def test_search_subjects(self, client):
-		factories.SubjectFactory.create()
-		response = client.get(reverse('search_subjects'))
-
+		subject = factories.SubjectFactory.create()
+		response = client.get(reverse('search_subjects') + '?q=' + subject.name)
 		assert response.status_code == 200
 		assert response.templates[0].name == 'subjects/subject_search.html'
+		assert b'Search for a UNTLBS Subject.' in response.content
+		assert '<span class="subject">{name}</span>'.format(name=subject.name).encode() in response.content
 
 	def test_about_subjects(self, client):
 		# Get the path of app path and the filename
